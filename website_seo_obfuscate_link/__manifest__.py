3fefb9bf551808b6232c4249704a174419889594
Xavier Brochard <xavier@alternatif.org>
2018-03-04 13:21:46 +0100

    add seo obfuscate link

---
+++ website_seo_obfuscate_link/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Xavier Brochard
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "SEO Obfuscate Links",
+    "summary": "Snippet to obfuscate links from search engines (for Page Rank Sculpture).",
+    "version": "10.0.1.0.0",
+    "category": "Website",
+    "website": "https://zeroheure.info/",
+    "author": "Xavier Brochard, zeroheure.info",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website",
+    ],
+    "data": [
+        "views/s_obfuscate.xml",
+        "views/assets.xml",
+    ],
+}
