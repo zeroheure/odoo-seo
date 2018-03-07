6da1c074e539709d5eac6ddbed2af07f7742a7f2
Xavier Brochard <xavier@alternatif.org>
2018-03-07 19:53:11 +0100

    working config for rte

--- website_seo_obfuscate_link/static/src/js/website_seo_obfuscate_link.js
+++ website_seo_obfuscate_link/static/src/js/website_seo_obfuscate_link.js
@@ -1,35 +1,31 @@
-odoo.define('website_seo_obfuscate_link', function (require) {
-/* FieldTextHtml Widget see module web_editor/static/src/js/backend.js */
+odoo.define('website_seo_obfuscate_link.rte', function (require) {
+    'use strict';
+    var ajax = require("web.ajax");
+    //var core = require("web.core");
+    var rte = require('web_editor.rte');
 
-var FieldTextHtmlSimple = web_editor.backend.widget.extend({
-    template: 'web_editor.FieldTextHtmlSimple',
-    _config: function () {
-        var self = this;
-        var config = {
-            'focus': false,
-            'height': 180,
-            'toolbar': [
-                ['style', ['style']],
-                ['font', ['bold', 'italic', 'underline', 'add-text-tags', 'clear']],
-                ['fontsize', ['fontsize']],
-                ['color', ['color']],
-                ['para', ['ul', 'ol', 'paragraph']],
-                ['table', ['table']],
-                ['insert', ['link', 'picture']],
-                ['history', ['undo', 'redo']]
-            ],
-            'prettifyHtml': false,
-            'styleWithSpan': false,
-            'inlinemedia': ['p'],
-            'lang': "odoo",
-            'onChange': function (value) {
-                self.internal_set_value(value);
-                self.trigger('changed_value');
-            }
-        };
-        if (session.debug) {
-            config.toolbar.splice(7, 0, ['view', ['codeview']]);
+    //ajax.loadJS('/website_seo_obfuscate_link/static/lib/summernote-add-text-tags.js');
+
+
+    rte.Class.include({
+        config: function ($editable) {
+            var config = this._super.apply(this, arguments);
+            config.plugin : {};
+            //config.plugin(require) {('summernote-add-text-tags.js');};
+                //{ ajax.loadJS('/website_seo_obfuscate_link/static/lib/summernote-add-text-tags.js'); };
+            config.airPopover.splice(1, 1, ['font', ['bold', 'italic', 'clear']]);
+            return config;
         }
-        return config;
-    },
-}
+    });
+
+
+    /* Backend
+    * FieldTextHtml Widget 
+    * see module 
+    * web_editor/static/src/js/backend.js 
+    */
+});
+
+
+
+
