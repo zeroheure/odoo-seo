091fd11d1d3b3c4519bce1ff1c8494b17dd15440
Xavier Brochard <xavier@alternatif.org>
2018-03-10 16:35:28 +0100

    first commit

---
+++ website_extend_web_editor/README.rst
@@ -0,0 +1,23 @@
+.. image:: https://img.shields.io/badge/licence-LGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/lgpl-3.0-standalone.html
+   :alt: License: LGPL-3
+
+======================
+Website Extend web editor
+======================
+
+Extend the web editor with default Summernotes options.
+
+
+Contributors
+------------
+
+* Xavier Brochard zeroheure@zeroheure.info
+
+Maintainer
+----------
+
+ZeroHeure
+https://zeroheure.info
+
+
---
+++ website_extend_web_editor/static/src/js/extend_rte.js
@@ -0,0 +1,30 @@
+odoo.define('website_extend_web_editor.rte', function (require) {
+    'use strict';
+    //var ajax = require("web.ajax");
+    //var core = require("web.core");
+    var rte = require('web_editor.rte');
+
+    //ajax.loadJS('/website_seo_obfuscate_link/static/lib/summernote-add-text-tags.js');
+
+
+    rte.Class.include({
+
+        config: function ($editable) {
+            var config = this._super.apply(this, arguments);
+            //config.plugin : {};
+            //config.plugin(require) {('summernote-add-text-tags.js');};
+                //{ ajax.loadJS('/website_seo_obfuscate_link/static/lib/summernote-add-text-tags.js'); };
+            config.airPopover.splice(1, 1, ['font', ['bold', 'italic', 'clear']]);
+            return config;
+        }
+    });
+
+
+    /* Backend
+    * FieldTextHtml Widget 
+    * see module 
+    * web_editor/static/src/js/backend.js 
+    */
+});
+
+
