0128ba69a837a8c1e0631d9aa86762fb3d1a9581
Xavier Brochard <xavier@alternatif.org>
2018-03-14 16:35:32 +0100

    add templates for blog, partners and customers explain purpose of obfuscated links

--- website_seo_footer_more/README.rst
+++ website_seo_footer_more/README.rst
@@ -3,10 +3,20 @@
    :alt: License: LGPL-3
 
 ======================
-Website SEO Payments Footer
+Website SEO Footer More
 ======================
 
-Remove Heading tag (H4) from payments list in footer.
+Remove Heading tag (H4) from payments method list in footer.
+Obfuscate additional links in footer :
+ 
+ * Our References
+ * News (i.e. blog)
+ * Resellers
+ * ...
+ * ...
+ * Payment Methods
+ 
+Purpose of obfuscated links is to not send "Page Rank juice" to each page linked in footer. Links are not obfuscated to visitors, only to search engines (as long as they ignore Javascript form).
 
 Contributors
 ------------
--- website_seo_footer_more/__manifest__.py
+++ website_seo_footer_more/__manifest__.py
@@ -2,8 +2,8 @@
 # Copyright 2018 Xavier Brochard
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
-    "name": "SEO Payments Footer",
-    "summary": "Remove Heading tag (H4) from payments list in footer.",
+    "name": "SEO Footer More",
+    "summary": "Remove Heading tag and obfuscate additional links in footer.",
     "version": "10.0.1.0.0",
     "category": "Website",
     "website": "https://zeroheure.info/",
@@ -13,9 +13,10 @@
     "installable": True,
     "depends": [
         "website_payment",
+        "website_blog",
         "website_seo_footer",
     ],
     "data": [
-        "views/seo_payment.xml",
+        "views/templates.xml",
     ],
 }
