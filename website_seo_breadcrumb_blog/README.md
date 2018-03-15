16ca8a261f949f89ac53f8983a01397b8a7392f4
Xavier Brochard <xavier@alternatif.org>
2018-03-15 15:50:39 +0100

    New module seo breadcrumb blog

---
+++ website_seo_breadcrumb_blog/README.md
@@ -0,0 +1,18 @@
+# Website SEO Breadcrumb Blog
+
+Odoo build breadcrumbs as a Bootstrap component. It miss structured datas.
+
+This module build breadcrumb with Heading tag (&lt;h6&gt;) to give more importance to its keywords.
+
+Additionaly, elements are structured with schema.org microdatas as recommended by Google. This will improve search result pages (SERP).
+
+## Contributors
+
+- Xavier Brochard zeroheure@zeroheure.info
+
+## Maintainer
+
+ZeroHeure
+https://zeroheure.info
+
+Please, report bugs at https://github.com/zeroheure/odoo-seo/issues
---
+++ website_seo_breadcrumb_blog/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Xavier Brochard
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "SEO Breadcrumb Blog",
+    "summary": "Build Breadcrumb with structured datas and heading tags",
+    "version": "10.0.1.0.0",
+    "category": "Website",
+    "website": "https://zeroheure.info/",
+    "author": "Xavier Brochard, zeroheure.info",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_blog",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+}
