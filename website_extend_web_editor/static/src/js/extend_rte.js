64cfea15e97dbdb359a439fd4e30d2c0bc4585c5
Xavier Brochard <xavier@alternatif.org>
2018-03-22 22:58:33 +0100

    add hr and line heiqgt

--- website_extend_web_editor/static/src/js/extend_rte.js
+++ website_extend_web_editor/static/src/js/extend_rte.js
@@ -1,20 +1,48 @@
-odoo.define('website_extend_web_editor.rte', function (require) {
+odoo.define('website_extend_web_editor.extend_rte', function (require) {
     'use strict';
     //var ajax = require("web.ajax");
     //var core = require("web.core");
     var rte = require('web_editor.rte');
 
+    
+//     context.memo('button.extend-rte', function () {
+//         return ui.buttonGroup([
+//             ui.button({
+//                 className: 'dropdown-toggle',
+//                 contents: '<b>+</b> ' + ui.icon(options.icons.caret, 'span'),
+//                 tooltip: 'Additional text styles',
+//                 data: {
+//                     toggle: 'dropdown'
+//                 }
+//             }),
+//             ui.dropdown([
+//                 ui.buttonGroup({
+//                     className: 'note-add-text-tags-code',
+//                     children: [strikethrough, superscript, subscript, hr, height, codeview]
+//                 }),
+//             ])
+//         ]).render();
+//     });
+    
     rte.Class.include({
 
         config: function ($editable) {
             var config = this._super.apply(this, arguments);
             config.airPopover.splice(1, 1, 
-                ['font', ['bold', 'italic', 'strikethrough', 'superscript', 'subscript', 'hr', 'clear']]);
+                ['font', ['bold', 'italic', 'underline', 'strikethrough', 'clear']]);
+            config.airPopover.splice(4, 1,
+                ['para', ['ul', 'ol', 'paragraph', 'height']]);
+            config.airPopover.splice(6, 1,
+                ['insert', ['link', 'picture', 'hr']]);
             return config;
         }
     });
 
-
+    // font 'strikethrough', 'superscript', 'subscript', 'fontname'
+    // para 'height'
+    // insert 'hr'
+    // autre 'codeview'
+    
     /* Backend
     * FieldTextHtml Widget 
     * see module 
