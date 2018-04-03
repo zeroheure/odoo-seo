3f8b85c5245b60afd7b1a4a296471b6c96c1f3c3
Xavier Brochard <xavier@alternatif.org>
2018-04-03 14:56:55 +0200

    field rame

--- website_extend_web_editor/models/summernote_config.py
+++ website_extend_web_editor/models/summernote_config.py
@@ -5,16 +5,16 @@
 from odoo import models, fields, api
 
 class SummernoteConfig(models.Model):
-    _name = 'website_extend_web_editor.summernote_config'
+    _name = 'summernote.config'
     _inherit = 'website.config.settings'
 
-    summernote_settings = fields.Boolean(
+    settings = fields.Boolean(
         string='Summernote web editor', 
         help="Show / hide Summernote additional parameters."
     )
-    summernote_strikethrough = fields.Boolean(string="Strike")
-    summernote_superscript = fields.Boolean(string="Sup")
-    summernote_subscript = fields.Boolean(string="Sub")
-    summernote_hr = fields.Boolean(string="Line (hr)")
-    summernote_height = fields.Boolean(string="Line height")
-    summernote_codeview = fields.Boolean(string="Show html")
+    strikethrough = fields.Boolean(string="Strike")
+    superscript = fields.Boolean(string="Sup")
+    subscript = fields.Boolean(string="Sub")
+    hr = fields.Boolean(string="Line (hr)")
+    height = fields.Boolean(string="Line height")
+    codeview = fields.Boolean(string="Show html")
