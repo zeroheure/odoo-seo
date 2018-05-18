4179d4fdb70c6877d4147c6690067c0ac36c3362
Xavier Brochard <xavier@alternatif.org>
2018-05-18 19:26:52 +0200

    Fix buttons

--- website_seo_semantic_tags/static/src/js/semantic_tags_rte.js
+++ website_seo_semantic_tags/static/src/js/semantic_tags_rte.js
@@ -6,17 +6,18 @@ odoo.define('website_seo_semantic_tags.semantic_tags_rte', function (require) {
     ajax.loadJS('/website_seo_semantic_tags/static/lib/summernote-semantic-tags.js');
 
     rte.Class.include({
-
+        // update rte.js config
         config: function ($editable) {
             var config = this._super.apply(this, arguments);
+            config.airPopover.splice(0, 1, 
+                ['style', ['style', 'strong', 'em', 'mark']]
+            );
             config.airPopover.splice(1, 1, 
-                ['font', ['strong', 'em', 'mark']],
                 ['sem', ['q', 'cite', 'abbr']],
                 ['change', ['del', 'ins']],
             );
-            config.airPopover.splice(7, 1, 
-                ['history', ['undo', 'redo']],
-                ['help', ['clear', 'codeview', 'help']],
+            config.airPopover.splice(8, 1, 
+                ['help', ['clear']],
             );
             return config;
         }
