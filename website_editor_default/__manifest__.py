7e996bc12f5cf291cd6368cf637e3d57943072bf
Xavier Brochard <xavier@alternatif.org>
2018-05-19 22:05:36 +0200

    create plugin file

--- website_editor_default/__manifest__.py
+++ website_editor_default/__manifest__.py
@@ -7,13 +7,13 @@
     "version": "10.0.0.1.0",
     "category": "Website",
     "website": "https://zeroheure.info/",
+    "support": "support@zeroheure.info",
     "author": "Xavier Brochard, zeroheure.info",
     "license": "LGPL-3",
     "application": False,
     "installable": True,
     "depends": [
         "web_editor",
-        
     ],
     "data": [
         "views/assets.xml",
---
+++ website_editor_default/static/lib/summernote-default-tags.js
@@ -0,0 +1,43 @@
+/**
+*    Copyright 2018 zeroheure.info - Xavier Brochard
+*    MIT License
+**/
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+     } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(window.jQuery);
+    }
+}(function ($) {
+    var tmpl = $.summernote.renderer.getTemplate();
+
+    /**
+    * @class plugin.default
+    */
+    $.summernote.addPlugin({
+
+        name: 'default',
+        buttons: {
+ 
+        more: function (lang, options) {
+                var tplDropdown = $.summernote.renderer.getTemplate().dropdown;
+                var tplButton = $.summernote.renderer.getTemplate().button;
+                var dropdown = [
+                    '<div class="note-align btn-group text-tags" style="min-width: 275px;">',
+                    strikethrough + superscript + subscript + hr + height,
+                    '</div>'
+                ];
+                return tplButton('+', {
+                    title: 'More icons',
+                    hide: true,
+                    dropdown: tplDropdown(dropdown, 'text-tags', 'ul')
+                });
+            },
+    });
+
+}));
+
+ 
