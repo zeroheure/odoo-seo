f31e4854d33fe52799a6231c55312ee1f77f8c6c
Xavier Brochard <xavier@alternatif.org>
2018-05-19 15:12:31 +0200

    complete module short description

--- website_seo_meta_size/__manifest__.py
+++ website_seo_meta_size/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "SEO Meta Size",
-    "summary": "Increase size of title and description fields.",
+    "summary": "Increase size of meta fields according to new Google rules.",
     "version": "10.0.1.0.0",
     "category": "Website",
     "website": "https://zeroheure.info/",
