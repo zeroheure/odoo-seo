31750e9695233ec14a28619a1f02fae8ab7ee711
Xavier Brochard <xavier@alternatif.org>
2018-05-19 14:53:44 +0200

    rename folder and module

--- website_editor_default/__manifest__.py
+++ website_editor_default/__manifest__.py
@@ -2,8 +2,8 @@
 # Copyright 2018 Xavier Brochard
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
-    "name": "Editor Complete",
-    "summary": "Enable all defaults options in website editor.",
+    "name": "Editor defaults options",
+    "summary": "Enable all defaults options in website editor (more icons).",
     "version": "10.0.0.1.0",
     "category": "Website",
     "website": "https://zeroheure.info/",
