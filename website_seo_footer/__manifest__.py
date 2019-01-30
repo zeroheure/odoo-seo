f995e08e292b103a8f21887ea0bd4fffb7327e67
Xavier Brochard <xavier@alternatif.org>
2019-01-30 23:04:00 +0100

    shorter summary

--- website_seo_footer/__manifest__.py
+++ website_seo_footer/__manifest__.py
@@ -3,16 +3,16 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "SEO Footer",
-    "summary": "Remove heading tags (H1, H2, ...) and obfuscate default links from search engines in footer (Page Rank Sculpture).",
+    "summary": "Remove heading tags and obfuscate default links in footer (Page Rank Sculpture).",
     "version": "10.0.1.0.0",
     "category": "Website",
     "website": "https://zeroheure.info/",
     "support" : "support@zeroheure.info",
     "author": "Xavier Brochard, zeroheure.info",
     "license": "LGPL-3",
-    "price":"100",
+    "price":"10",
     "currency":"EUR",
-    "images": ['images/main_screenshot.png'],
+    "images": ['static/description/module_image.png'],
     "application": False,
     "installable": True,
     "depends": [
