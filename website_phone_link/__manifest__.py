5a959ad620d3fdd919d464cef5abee2513e02bef
Xavier Brochard <xavier@alternatif.org>
2019-01-31 22:01:14 +0100

    manifest formating

--- website_phone_link/__manifest__.py
+++ website_phone_link/__manifest__.py
@@ -2,24 +2,26 @@
 # Copyright 2018 Xavier Brochard
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
-    "name": "Click and call",
-    "summary": "Click and call phone number for mobile usability.",
-    "version": "10.0.1.0.0",
-    "category": "Website",
-    "website": "https://zeroheure.info/",
-    "support" : "support@zeroheure.info",
-    "author": "Xavier Brochard, zeroheure.info",
-    "license": "LGPL-3",
-    "price":"20",
-    "currency":"EUR",
-    "images": ['static/description/module_image.png'],
-    "application": False,
-    "installable": True,
-    "depends": [
-        "website",
+    'name': 'Click and call',
+    'summary': 'Click and call phone number for mobile usability.',
+    'version': '10.0.1.0.0',
+    'category': 'Website',
+    'website': 'https://zeroheure.info/',
+    'support' : 'support@zeroheure.info',
+    'author': 'Xavier Brochard, zeroheure.info',
+    'license': 'LGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website',
     ],
-    "data": [
-        "views/layout.xml",
-        "views/ir_qweb.xml",
+    'data': [
+        'views/layout.xml',
+        'views/ir_qweb.xml',
     ],
+    'images': [
+        'static/description/module_image.png',
+    ],
+    'price': 20,
+    'currency':'EUR',
 }
