8cea63fabf7157715508a29942f44536c1501dc3
Xavier Brochard <xavier@alternatif.org>
2019-02-01 15:36:51 +0100

    remove price, freely available modules
    # Conflicts:
    #	website_phone_link/__manifest__.py
    #	website_seo_meta/__manifest__.py

--- website_seo_meta/__manifest__.py
+++ website_seo_meta/__manifest__.py
@@ -2,23 +2,24 @@
 # Copyright 2018 Xavier Brochard
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
-    "name": "SEO Meta",
-    "summary": "Improve meta fields according to new Google rules.",
-    "version": "10.0.1.0.0",
-    "category": "Website",
-    "website": "https://zeroheure.info/",
-    "support" : "support@zeroheure.info",
-    "author": "Xavier Brochard, zeroheure.info",
-    "license": "LGPL-3",
-    "price":"20",
-    "currency":"EUR",
-    "images": ['static/description/module_image.png'],
-    "application": False,
-    "installable": True,
-    "depends": [
-        "website",
+    'name': 'SEO Meta',
+    'summary': 'Improve meta fields according to new Google rules.',
+    'version': '10.0.1.0.0',
+    'category': 'Website',
+    'website': 'https://zeroheure.info/',
+    'support' : 'support@zeroheure.info',
+    'author': 'Xavier Brochard, zeroheure.info',
+    'license': 'LGPL-3',
+    'images': ['static/description/module_image.png'],
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website',
     ],
-    "data": [
-        "views/assets.xml",
+    'data': [
+        'views/assets.xml',
+    ],
+    'images': [
+        'static/description/module_image.png',
     ],
 }
