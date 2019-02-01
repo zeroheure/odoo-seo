c2f63d063052b9aa4f6342f7426497410ea85946
Xavier Brochard <xavier@alternatif.org>
2019-02-01 18:09:27 +0100

    release module

---
+++ website_phone_link_treehouse/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Xavier Brochard
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'Treehouse Click And Call',
+    'summary': 'Click and call for Treehouse, Zap and Bistro themes.',
+    'version': '9.0.1.0.0',
+    'category': 'Website',
+    'website': 'https://zeroheure.info/',
+    'support' : 'support@zeroheure.info',
+    'author': 'Xavier Brochard, zeroheure.info',
+    'license': 'LGPL-3',
+    'images': ['static/description/module_image.png'],
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website',
+        'theme_treehouse',
+    ],
+    'data': [
+        'views/layout.xml',
+        'views/ir_qweb.xml',
+    ],
+    'images': [
+        'static/description/module_image.png',
+    ],
+    'price': 10,
+    'currency': 'EUR',
+}
