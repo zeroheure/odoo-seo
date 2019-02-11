02def4172147fcfac1434652a78de9177995abcd
Xavier Brochard <xavier@alternatif.org>
2019-02-11 17:39:32 +0100

    single quotes

--- website_seo_footer/__manifest__.py
+++ website_seo_footer/__manifest__.py
@@ -2,24 +2,24 @@
 # Copyright 2018 Xavier Brochard
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
-    "name": "SEO Footer",
-    "summary": "Remove heading tags and obfuscate default links in footer (Page Rank Sculpture).",
-    "version": "10.0.0.0.1",
-    "category": "Website",
-    "website": "https://zeroheure.info/",
-    "support" : "support@zeroheure.info",
-    "author": "Xavier Brochard, zeroheure.info",
-    "license": "LGPL-3",
-    "price":"10",
-    "currency":"EUR",
-    "images": ['static/description/module_image.png'],
-    "application": False,
-    "installable": True,
-    "depends": [
-        "website",
+    'name': 'SEO Footer',
+    'summary': 'Remove heading tags and obfuscate default links in footer (Page Rank Sculpture).',
+    'version': '10.0.0.0.1',
+    'category': 'Website',
+    'website': 'https://zeroheure.info/',
+    'support' : 'support@zeroheure.info',
+    'author': 'Xavier Brochard, zeroheure.info',
+    'license': 'LGPL-3',
+    'price':'10',
+    'currency':'EUR',
+    'images': ['static/description/module_image.png'],
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website',
     ],
-    "data": [
-        "views/seo_footer.xml",
-        "views/assets.xml",
+    'data': [
+        'views/seo_footer.xml',
+        'views/assets.xml',
     ],
 }
