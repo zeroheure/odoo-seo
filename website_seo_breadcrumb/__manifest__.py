08f37ea642374335c5de0fc59bef853f10af6037
Xavier Brochard <xavier@alternatif.org>
2019-02-14 17:07:46 +0100

    add seo footer and seo breadcrumb

---
+++ website_seo_breadcrumb/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Xavier Brochard
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'SEO Breadcrumb',
+    'summary': 'Build Breadcrumb with structured datas and heading tags',
+    'version': '11.0.1.0.0',
+    'category': 'Website',
+    'website': 'https://zeroheure.info/',
+    'support' : 'support@zeroheure.info',
+    'author': 'Xavier Brochard, zeroheure.info',
+    'license': 'LGPL-3',
+    'images': ['images/main_screenshot.png'],
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+}
