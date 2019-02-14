b46492a95c32839f06991c08d4f3fdab1a2bbd21
Xavier Brochard <xavier@alternatif.org>
2019-02-14 17:03:11 +0100

    add modules seo footer and seo meta

---
+++ website_seo_footer/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Xavier Brochard
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    'name': 'SEO Footer',
+    'summary': 'Remove heading tags and obfuscate default links in footer (Page Rank Sculpture).',
+    'version': '10.0.1.0.0',
+    'category': 'Website',
+    'website': 'https://zeroheure.info/',
+    'support' : 'support@zeroheure.info',
+    'author': 'Xavier Brochard, zeroheure.info',
+    'license': 'LGPL-3',
+    'images': ['static/description/module_image.png'],
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/seo_footer.xml',
+        'views/assets.xml',
+    ],
+}
