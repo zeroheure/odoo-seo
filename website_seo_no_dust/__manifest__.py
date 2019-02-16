e4ec216359d47f77d7bbfe55bdc6908fe9c1b98f
Xavier Brochard <xavier@alternatif.org>
2019-02-16 16:29:12 +0100

    quotes, version, module image

--- website_seo_no_dust/__manifest__.py
+++ website_seo_no_dust/__manifest__.py
@@ -2,23 +2,25 @@
 # Copyright 2018 Xavier Brochard
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
-    "name": "SEO no DUST",
-    "summary": "Remove URL parameters on product page to avoid Duplicate Content",
-    "version": "10.0.0.0.1",
-    "category": "Website",
-    "website": "https://zeroheure.info/",
-    "support" : "support@zeroheure.info",
-    "author": "Xavier Brochard, zeroheure.info",
-    "license": "LGPL-3",
-    "price":"100",
-    "currency":"EUR",
-    "images": ['images/main_screenshot.png'],
-    "application": False,
-    "installable": True,
-    "depends": [
-        "website_sale",
+    'name': 'SEO no DUST',
+    'summary': 'Remove URL parameters on product page to avoid Duplicate Content',
+    'version': '10.0.1.0.1',
+    'category': 'Website',
+    'website': 'https://zeroheure.info/',
+    'support' : 'support@zeroheure.info',
+    'author': 'Xavier Brochard, zeroheure.info',
+    'license': 'LGPL-3',
+    'price':'100',
+    'currency':'EUR',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website_sale',
     ],
-    "data": [
-        "views/templates.xml",
+    'data': [
+        'views/templates.xml',
+    ],
+    'images': [
+        'static/description/module_image.png',
     ],
 }
