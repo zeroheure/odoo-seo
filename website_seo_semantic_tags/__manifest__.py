9607e31df72d04471b4341da12cc03f8addfd5fc
Xavier Brochard <xavier@alternatif.org>
2019-02-21 11:21:26 +0100

    quotes

--- website_seo_semantic_tags/__manifest__.py
+++ website_seo_semantic_tags/__manifest__.py
@@ -2,24 +2,26 @@
 # Copyright 2018 Xavier Brochard
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
-    "name": "SEO Semantic Tags",
-    "summary": "Extend the website editor with semantic tags for SEO.",
-    "version": "10.0.0.0.1",
-    "category": "Website",
-    "website": "https://zeroheure.info/",
-    "support" : "support@zeroheure.info",
-    "author": "Xavier Brochard, zeroheure.info",
-    "license": "LGPL-3",
-    "price":"100",
-    "currency":"EUR",
-    "images": ['images/main_screenshot.png'],
-    "application": False,
-    "installable": True,
-    "depends": [
-        "website",
-        "web_editor",
+    'name': 'SEO Semantic Tags',
+    'summary': 'Extend the website editor with semantic tags for SEO.',
+    'version': '10.0.0.0.1',
+    'category': 'Website',
+    'website': 'https://zeroheure.info/',
+    'support' : 'support@zeroheure.info',
+    'author': 'Xavier Brochard, zeroheure.info',
+    'license': 'LGPL-3',
+    'price':'100',
+    'currency':'EUR',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website',
+        'web_editor',
     ],
-    "data": [
-        "views/assets.xml",
+    'data': [
+        'views/assets.xml',
+    ],
+    'images': [
+        'static/description/module_image.png',
     ],
 }
