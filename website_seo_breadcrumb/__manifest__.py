4054e2f5258c42cae197072a7755c282be2dab02
Xavier Brochard <xavier@alternatif.org>
2019-04-01 14:53:18 +0200

    small typos

--- website_seo_breadcrumb/__manifest__.py
+++ website_seo_breadcrumb/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     'name': 'SEO Breadcrumb',
-    'summary': 'Build Breadcrumb with structured datas and heading tags',
+    'summary': 'Build Breadcrumb with structured datas and heading tags.',
     'version': '11.0.1.0.0',
     'category': 'Website',
     'website': 'https://zeroheure.info/',
